10f2d7d62a92d657391e5b827ae2f2dbaa2d2d49
murakawa <s-murakawa@nri-net.com>
2019-10-28 15:20:41 +0900

    first commit

---
+++ source/stop-instance.py
@@ -0,0 +1,20 @@
+import os
+import time
+import googleapiclient.discovery
+import json
+import base64
+
+def start_instance(compute, project, zone, instance_name):
+    return compute.instances().stop(
+		project=project,
+		zone=zone,
+		instance=instance_name).execute()
+
+def main(event, context):
+	data = json.loads(base64.b64decode(event['data']).decode('utf8'))
+	instance_name = data["instance_name"]
+	zone = data["zone"]
+	project = "INPUT PROJECT NAME"
+
+	compute = googleapiclient.discovery.build('compute', 'v1')
+	start_instance(compute, project, zone, instance_name)
